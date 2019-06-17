78cef2e09cab838853584d6d05780a2e2b8c4cbb
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-17 06:53:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,9 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty"
       else
-        resp.write @@cart
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
       end
     elsif req.path.match(/add/)
       item = req.params["item"]
