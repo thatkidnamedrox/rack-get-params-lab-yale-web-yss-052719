cf571cc78de287ab98a451a09cee8cbe6b7047a8
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-17 06:54:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,6 +28,7 @@ class Application
         @@cart << item
         resp.write "added #{item}"
       else
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
